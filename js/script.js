7629a7cffd703e62dfb66a09484873bb3e8816be
COPSTA <tutomx16@gmail.com>
2024-04-16 13:22:24 -0500

    Añade funcion al js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/**VARIABLES */
+
+
+
+
+/**FUNCIONES*/
+
+const calculate = () => {
+    const value = document.querySelector("#numbers").value;
+    const array = value.split(/,\s*/g);
+    const numbers = array.map(el=> Number(el)).filter(el => !isNaN(el));
+
+}
