16a6f8c52d441faf4b06f5bdbd3869791cac9f52
COPSTA <tutomx16@gmail.com>
2024-04-16 17:42:59 -0500

    Añade funciones de calcular el mean y la media

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 /**VARIABLES */
 
-
-
-
 /**FUNCIONES*/
 
-const calculate = () => {
-    const value = document.querySelector("#numbers").value;
-    const array = value.split(/,\s*/g);
-    const numbers = array.map(el=> Number(el)).filter(el => !isNaN(el));
-
+const getMean = (array) =>  array.reduce((acc, el) => acc + el, 0) / array.length;
+const getMedian = (array) =>{
+  const sorted = array.sort(a,b=> a-b);
+  const median = array.length % 2 === 0? 
+    getMean([sorted[array.length / 2],sorted[array.length / 2 - 1]]): 
+    sorted[Math.floor(array.length / 2)];
+    return median;
 }
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(",");
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+  const mean = getMean(numbers);
+  document.querySelector("#mean").textContent = mean;
+};
