9d53c9228f75b098dbade22a4d8147eaafea0f9b
COPSTA <tutomx16@gmail.com>
2024-04-17 14:38:51 -0500

    Añade funcion de mode

--- js/script.js
+++ js/script.js
@@ -3,14 +3,32 @@
 /**FUNCIONES*/
 
 const getMean = (array) =>  array.reduce((acc, el) => acc + el, 0) / array.length;
+
 const getMedian = (array) =>{
-  debugger
   const sorted = array.sort((a,b) => a-b);
   const median = array.length % 2 === 0? 
     getMean([sorted[array.length / 2],sorted[array.length / 2 - 1]]): 
     sorted[Math.floor(array.length / 2)];
     return median;
 }
+
+const getMode = (array) =>{
+  const counts = {};
+  array.forEach(el => {
+    if(counts[el]){
+      debugger
+      counts[el] +=1;
+    }else{
+      counts[el] = 1;
+    }
+    
+  });
+  if(new Set(counts.values()).size = 1 ){
+    return null
+  }
+}
+
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(",");
@@ -19,5 +37,5 @@ const calculate = () => {
   const median = getMedian(numbers);
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
-  
+
 };
